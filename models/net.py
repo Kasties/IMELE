13fb13720a34d0d40b8944afb578b648e8251f76
Kasties <59847033+Kasties@users.noreply.github.com>
2023-02-09 11:19:00 +0100

    Update net.py

--- models/net.py
+++ models/net.py
@@ -28,8 +28,8 @@ class model(nn.Module):
     def forward(self, x):
         x_block0, x_block1, x_block2, x_block3, x_block4 = self.E(x)
   
-        x= x_block0.view(-1,250,250)
-
+       # x= x_block0.view(-1,250,250)
+        x = x_block0.view(-1, 440, 440)
         x = x.cpu().detach().numpy()
         
         #for idx in range(0,len(x)):
